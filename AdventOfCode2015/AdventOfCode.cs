34ada36bbe17c54572b876cc70db60734e76692d
Jörgen Bergström <jorgen@bergstrom.nu>
2016-12-07 20:58:50 +0100

    AoC 2015 started

---
+++ AdventOfCode2015/AdventOfCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class AdventOfCode
+    {
+        static void Main()
+        {
+            First.Run();
+
+            Console.WriteLine("Merry Christmas...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2015/First.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class First
+    {
+        public static void Run()
+        {
+            string line;
+            StreamReader file = new StreamReader(@".\Data\First.txt");
+
+            List<string> strings = new List<string>();
+
+            int numberOfUps = 0;
+            int numberOfDowns = 0;
+            int level = 0;
+            int position = 1;
+            while ((line = file.ReadLine()) != null)
+            {
+                numberOfUps = numberOfUps + line.Count(f => f == '(');
+                numberOfDowns = numberOfDowns + line.Count(f => f == ')');
+
+                foreach(char c in line)
+                {
+                    if (c == '(')
+                    {
+                        level++;
+                    }
+                    else if (c == ')')
+                    {
+                        level--;
+                    }
+
+                    if (level < 0)
+                    {
+                        Console.WriteLine(position);
+                        break;
+                    }
+                    position++;
+                }
+            }
+            Console.WriteLine(numberOfUps - numberOfDowns);
+       }
+    }
+}
