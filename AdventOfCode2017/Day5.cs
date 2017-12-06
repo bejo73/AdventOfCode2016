21bc5691aec2375e20ae2049c9ba3d4a3b044430
Jörgen Bergström <jorgen@bergstrom.nu>
2017-12-06 19:57:07 +0100

    Day5 & Day6

---
+++ AdventOfCode2017/Day5.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2017
+{
+    class Day5
+    {
+        public static void Run()
+        {
+            string line;
+            StreamReader file = new StreamReader(@".\Data\5.txt");
+
+            List<int> maze = new List<int>();
+            List<int> maze2 = new List<int>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                maze.Add(Int32.Parse(line));
+                maze2.Add(Int32.Parse(line));
+            }
+
+            // Part 1
+            int position = 0;
+            int steps = 0;
+
+            while (0 <= position && position < maze.Count)
+            {
+                int offset = maze[position];
+
+                maze[position]++;
+
+                position = position + offset;
+                steps++;
+            }
+
+            Console.WriteLine("Day5 (1): " + steps);
+
+            // Part 2
+            position = 0;
+            steps = 0;
+
+            while (0 <= position && position < maze2.Count)
+            {
+                int offset = maze2[position];
+
+                if (offset >= 3)
+                {
+                    maze2[position]--;
+                }
+                else
+                {
+                    maze2[position]++;
+                }
+
+                position = position + offset;
+                steps++;
+            }
+
+            Console.WriteLine("     (2): " + steps);
+        }
+    }
+}
---
+++ AdventOfCode2017/Day6.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    class Day6
+    {
+        public static void Run()
+        {
+            //int[] bank = { 0, 2, 7, 0 };
+            //StringBuilder key = new StringBuilder("0-2-7-0");
+
+            int[] bank = { 4, 10, 4, 1, 8, 4, 9, 14, 5, 1, 14, 15, 0, 15, 3, 5 };
+            String key = getId(bank);
+
+            List<string> list = new List<string>();
+            list.Add(key);
+
+            HashSet<string> hash = new HashSet<string>();
+
+            int steps = 0;
+           
+            while (hash.Add(key))
+            {    
+                int max = bank.Max();
+                int index = bank.ToList().IndexOf(max);
+
+                int d = max / bank.Length ;
+
+                // Clear
+                bank[index] = 0;
+
+                // No need to add if zero
+                if (d > 0)
+                {
+                    for (int i = 0; i < bank.Length; i++)
+                    {
+                        bank[i] = bank[i] + d;
+                    }
+                }
+
+                for (int k = 1; k <= max % bank.Length; k++)
+                {
+                    if ((index + k) < bank.Length)
+                    {
+                        bank[index+k] = bank[index+k] + 1;
+                    }
+                    else
+                    {
+                        bank[index + k - bank.Length] = bank[index + k - bank.Length] + 1;
+                    }
+                }
+
+                key = getId(bank);
+                list.Add(key.ToString());
+                steps++;
+            }
+
+            int cycles = list.Count - list.IndexOf(key.ToString()) - 1;
+
+            Console.WriteLine("Day5 (1): " + steps);
+            Console.WriteLine("     (2): " + cycles);
+        }
+
+        private static string getId(int[] bank)
+        {
+            StringBuilder id = new StringBuilder();
+
+            for (int j = 0; j < bank.Length; j++)
+            {
+                id.Append(bank[j]);
+                id.Append("-");
+            }
+
+            id.Remove(id.Length - 1, 1);
+
+            return id.ToString();
+        }
+
+    }
+}
