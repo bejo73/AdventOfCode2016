d3c24c28d2ec653b8287d4ac51e79dee32b00188
Jörgen Bergström <jorgen@bergstrom.nu>
2017-12-06 19:56:51 +0100

    Day5 & Day6

--- AdventOfCode2017/AdventOfCode.cs
+++ AdventOfCode2017/AdventOfCode.cs
@@ -9,7 +9,9 @@ namespace AdventOfCode2017
             //Day1.Run();
             //Day2.Run();
             //Day3.Run();
-            Day4.Run();
+            //Day4.Run();
+            Day5.Run();
+            //Day6.Run();
 
             Console.WriteLine("Merry Christmas...");
             Console.ReadKey();
