f5ed1e5c2b594814a1b51624dcec0fc0e53e6ef9
Jörgen Bergström <jorgen@bergstrom.nu>
2017-12-19 21:34:18 +0100

    Prettyfied solution

--- AdventOfCode2017/Day19.cs
+++ AdventOfCode2017/Day19.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode2017
 
         public static void Run()
         {
-            StreamReader file = new StreamReader(@".\Data\19_Test_1.txt");
+            StreamReader file = new StreamReader(@".\Data\19.txt");
             string line;
 
             List<string> lines = new List<string>();
@@ -23,28 +23,26 @@ namespace AdventOfCode2017
 
             char[,] diagram = new char[lines[0].Length, lines.Count];
 
-            for (int y = 0; y < lines.Count;  y++)
+            for (int y = 0; y < lines.Count; y++)
             {
                 string l = lines[y];
                 for (int x = 0; x < l.Length; x++)
                 {
-                    diagram[x, lines.Count - 1 -y] = l[x];
+                    diagram[x, lines.Count - 1 - y] = l[x];
                 }
             }
 
-            int currentY = diagram.GetLength(1) - 1; 
+            int currentY = diagram.GetLength(1) - 1;
             int currentX = 0;
 
             int empty = 32;
 
-            Console.WriteLine("Start: x=" + currentX + ", y=" + currentY);
-
+            // Find starting x position
             for (int f = 0; f < diagram.GetLength(0); f++)
             {
                 if (diagram[f, currentY] == '|')
                 {
                     currentX = f;
-                    //Console.WriteLine("x: " + currentX + ", y: " + currentY);
                     break;
                 }
             }
@@ -55,353 +53,96 @@ namespace AdventOfCode2017
 
             int steps = 0;
 
-            while (true)
+            bool ready = false;
+            while (!ready)
             {
-                Console.WriteLine("x: " + currentX + ", y: " + currentY);
+                //Console.WriteLine("x: " + currentX + ", y: " + currentY);
                 steps++;
+
                 switch (direction)
                 {
                     case Direction.East:
                         currentX++;
 
-                        // East border
-                        if (currentX == diagram.GetLength(0))
+                        if (diagram[currentX, currentY] == '+')
                         {
-                            Console.WriteLine("East border - not implemented");
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            if (diagram[currentX, currentY] == '+')
+                            if (diagram[currentX, currentY + 1] != empty)
                             {
-                                if (currentY == 0)
-                                {
-                                    if (diagram[currentX, currentY + 1] != empty)
-                                    {
-                                        direction = Direction.North;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else if (currentY == diagram.GetLength(1))
-                                {
-                                    if (diagram[currentX, currentY - 1] != empty)
-                                    {
-                                        direction = Direction.South;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else
-                                {
-                                    if (diagram[currentX, currentY + 1] != empty)
-                                    {
-                                        direction = Direction.North;
-                                    }
-                                    else if (diagram[currentX, currentY - 1] != empty)
-                                    {
-                                        direction = Direction.South;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-
-                                    // Continue
-                                    if (char.IsLetter(diagram[currentX, currentY]))
-                                    {
-                                        letters.Append(diagram[currentX, currentY]);
-                                    }
-                                }
+                                direction = Direction.North;
                             }
-                            else
+                            else if (diagram[currentX, currentY - 1] != empty)
                             {
-                                if (char.IsLetter(diagram[currentX, currentY]))
-                                {
-                                    letters.Append(diagram[currentX, currentY]);
-                                }
-
-                                // Stop
-                                if (diagram[currentX, currentY] == empty)
-                                {
-                                    Console.WriteLine("East stop: " + letters.ToString());
-                                    Console.WriteLine("Steps: " + steps);
-                                    Console.ReadKey();
-                                }
-
+                                direction = Direction.South;
                             }
                         }
+
                         break;
                     case Direction.North:
                         currentY++;
 
-                        // North border
-                        if (currentY == diagram.GetLength(1))
+                        if (diagram[currentX, currentY] == '+')
                         {
-                            Console.WriteLine("North border - not implemented");
-                            Console.WriteLine(letters.ToString());
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            if (diagram[currentX, currentY] == '+')
+                            if (diagram[currentX + 1, currentY] != empty)
                             {
-                                if (currentX == 0)
-                                {
-                                    if (diagram[currentX + 1, currentY] != empty)
-                                    {
-                                        direction = Direction.East;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else if (currentX == diagram.GetLength(0))
-                                {
-                                    if (diagram[currentX - 1, currentY] != empty)
-                                    {
-                                        direction = Direction.West;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else
-                                {
-
-                                    if (diagram[currentX + 1, currentY] != empty)
-                                    {
-                                        direction = Direction.East;
-                                    }
-                                    else if (diagram[currentX - 1, currentY] != empty)
-                                    {
-                                        direction = Direction.West;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-
-                                    // Continue
-                                    if (char.IsLetter(diagram[currentX, currentY]))
-                                    {
-                                        letters.Append(diagram[currentX, currentY]);
-                                    }
-                                }
+                                direction = Direction.East;
                             }
-                            else
+                            else if (diagram[currentX - 1, currentY] != empty)
                             {
-                                // Continue
-                                if (char.IsLetter(diagram[currentX, currentY]))
-                                {
-                                    letters.Append(diagram[currentX, currentY]);
-                                }
+                                direction = Direction.West;
                             }
                         }
+
 
                         break;
                     case Direction.West:
                         currentX--;
 
-                        // West border
-                        if (currentX == 0)
+                        if (diagram[currentX, currentY] == '+')
                         {
-                            Console.WriteLine("West border - not implemented");
-                            Console.WriteLine(letters.ToString());
-                            Console.WriteLine("Steps: "+ steps);
-                            Console.ReadKey();
-                        }
-                        else
-                        {
-                            if (diagram[currentX, currentY] == '+')
+                            if (diagram[currentX, currentY + 1] != empty)
                             {
-                                if (currentY == 0)
-                                {
-                                    if (diagram[currentX, currentY + 1] != empty)
-                                    {
-                                        direction = Direction.North;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else if (currentY == diagram.GetLength(1))
-                                {
-                                    if (diagram[currentX, currentY - 1] != empty)
-                                    {
-                                        direction = Direction.South;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else
-                                {
-                                    if (diagram[currentX, currentY + 1] != empty)
-                                    {
-                                        direction = Direction.North;
-                                    }
-                                    else if (diagram[currentX, currentY - 1] != empty)
-                                    {
-                                        direction = Direction.South;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
+                                direction = Direction.North;
+                            }
+                            else if (diagram[currentX, currentY - 1] != empty)
+                            {
+                                direction = Direction.South;
+                            }
+                        }
 
-                                    // Continue
-                                    if (char.IsLetter(diagram[currentX, currentY]))
-                                    {
-                                        letters.Append(diagram[currentX, currentY]);
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                // Continue
-                                if (char.IsLetter(diagram[currentX, currentY]))
-                                {
-                                    letters.Append(diagram[currentX, currentY]);
-                                }
-                            }
-                        }
                         break;
 
                     case Direction.South:
                         currentY--;
 
-                        // South border
-                        if (currentY == 0)
+                        if (diagram[currentX, currentY] == '+')
                         {
-                            Console.WriteLine("South border");
-                            Console.WriteLine(letters.ToString());
-                            //Console.ReadKey();
-
-                            if (currentX == 0)
+                            if (diagram[currentX + 1, currentY] != empty)
                             {
-                                if (diagram[currentX + 1, currentY] != empty)
-                                {
-                                    direction = Direction.East;
-                                }
-                                else
-                                {
-                                    // Stop
-                                }
+                                direction = Direction.East;
                             }
-                            else if (currentX == diagram.GetLength(0))
+                            else if (diagram[currentX - 1, currentY] != empty)
                             {
-                                if (diagram[currentX - 1, currentY] != empty)
-                                {
-                                    direction = Direction.West;
-                                }
-                                else
-                                {
-                                    // Stop
-                                }
-                            }
-                            else
-                            {
-
-                                if (diagram[currentX + 1, currentY] != empty)
-                                {
-                                    direction = Direction.East;
-                                }
-                                else if (diagram[currentX - 1, currentY] != empty)
-                                {
-                                    direction = Direction.West;
-                                }
-                                else
-                                {
-                                    // Stop
-                                }
-
-                                // Continue
-                                if (char.IsLetter(diagram[currentX, currentY]))
-                                {
-                                    letters.Append(diagram[currentX, currentY]);
-                                }
-                            }
-
-
-
-
-                        }
-                        else
-                        {
-                            if (diagram[currentX, currentY] == '+')
-                            {
-                                if (currentX == 0)
-                                {
-                                    if (diagram[currentX + 1, currentY] != empty)
-                                    {
-                                        direction = Direction.East;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else if (currentX == diagram.GetLength(0))
-                                {
-                                    if (diagram[currentX - 1, currentY] != empty)
-                                    {
-                                        direction = Direction.West;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-                                }
-                                else
-                                {
-
-                                    if (diagram[currentX + 1, currentY] != empty)
-                                    {
-                                        direction = Direction.East;
-                                    }
-                                    else if (diagram[currentX - 1, currentY] != empty)
-                                    {
-                                        direction = Direction.West;
-                                    }
-                                    else
-                                    {
-                                        // Stop
-                                    }
-
-                                    // Continue
-                                    if (char.IsLetter(diagram[currentX, currentY]))
-                                    {
-                                        letters.Append(diagram[currentX, currentY]);
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                // Continue
-                                if (char.IsLetter(diagram[currentX, currentY]))
-                                {
-                                    letters.Append(diagram[currentX, currentY]);
-                                }
+                                direction = Direction.West;
                             }
                         }
-
 
                         break;
                 }
 
+                if (char.IsLetter(diagram[currentX, currentY]))
+                {
+                    letters.Append(diagram[currentX, currentY]);
+                }
+
+                // Stop
+                if (diagram[currentX, currentY] == empty)
+                {
+                    ready = true;
+                }
             }
 
-            Console.WriteLine("Day 3 (1): ");
-            Console.WriteLine("      (2): ");
+            Console.WriteLine("Day 19 (1): " + letters.ToString());
+            Console.WriteLine("       (2): " + steps);
         }
     }
 }
